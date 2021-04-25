92861e02d49f4edf0bac2257b3cb17fd179dd579
Ethan <ephelpse@icloud.com>
2021-04-25 12:59:51 +0100

    New splash page

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<p>Something's happening</p>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>felpsey</title>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
+        <meta name="author" content="Ethan Phelps">
+        <meta name="keywords" content="Website design,Web design,Blog,Projects">
+        <meta name="description" content="developer, designer, writing related to moral & ethical policing">
+
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://felpsey.com/">
+        <meta property="og:title" content="felpsey">
+        <meta property="og:description" content="developer, designer, writing related to moral & ethical policing">
+        <!-- <meta property="og:image" content="/banner.jpg"> -->
+
+        <meta property="twitter:url" content="https://felpsey.com/">
+        <meta property="twitter:title" content="Ethan Phelps">
+        <meta property="twitter:description" content="developer, designer, writing related to moral & ethical policing">
+        <!-- <meta property="twitter:card" content="summary_large_image"> -->
+        <!-- <meta property="twitter:image" content="/banner.jpg"> -->
+
+        <link rel="stylesheet" type="text/css" href="./public/css/index.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <!-- <img id="img" src="./public/img/macintosh-128k.svg"> -->
+
+            <svg id="macintosh" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320.7 443.61"><defs/><defs><style>.cls-1{fill:none;stroke-miterlimit:10}</style></defs><path class="cls-1" d="M9.23 386.69h302.24v50.42a6 6 0 01-6 6H15.23a6 6 0 01-6-6v-50.42z" style="animation:PlfXLEcr_draw 5333ms ease 0ms forwards" stroke-dasharray="713 715" stroke-dashoffset="714"/><path class="cls-1" d="M14.6 393.41h291.49v37a6 6 0 01-6 6H20.6a6 6 0 01-6-6v-37z" style="animation:PlfXLEcr_draw 5333ms ease 296ms forwards" stroke-dasharray="664 666" stroke-dashoffset="665"/><path class="cls-1" d="M237.23 405.1h47.73a3 3 0 013 3v19.24a3 3 0 01-3 3h-47.73a3 3 0 01-3-3V408.1a3 3 0 013-3" style="animation:PlfXLEcr_draw 5333ms ease 592ms forwards" stroke-dasharray="153 155" stroke-dashoffset="154"/><path class="cls-1" d="M269.87 409.24h12.35a1 1 0 011 1v14.95a1 1 0 01-1 1h-12.35a1 1 0 01-1-1v-14.95a1 1 0 011-1" style="animation:PlfXLEcr_draw 5333ms ease 888ms forwards" stroke-dasharray="61 63" stroke-dashoffset="62"/><path class="cls-1" d="M391.21 32.53v-.05l-.13-.15-.19-.18c-.09-.1-.17-.21-.27-.31a10.23 10.23 0 00-7.48-3.11h-63.02a6.81 6.81 0 00-7 6.6v2.8H188.21V35.3a6.81 6.81 0 00-7-6.6h-62.92a10.25 10.25 0 00-7.49 3.11l-.26.31-.2.18-.12.15a.1.1 0 000 .05L95.58 53.07l16.22 11.5 5.43-7.66h266.88l5.43 7.66 16.23-11.5z" transform="translate(-89.65 -28.2)" style="animation:PlfXLEcr_draw 5333ms ease 1185ms forwards" stroke-dasharray="673 675" stroke-dashoffset="674"/><path class="cls-1" d="M400.45 47.5H99.55a9.4 9.4 0 00-9.4 9.41v354.62A13.43 13.43 0 00103.58 425h292.84a13.43 13.43 0 0013.43-13.43V56.91a9.4 9.4 0 00-9.4-9.41zm-26.2 239.84H125.89V95.19h248.36z" transform="translate(-89.65 -28.2)" style="animation:PlfXLEcr_draw 5333ms ease 1481ms forwards" stroke-dasharray="2256 2258" stroke-dashoffset="2257"/><path class="cls-1" d="M281.91 323.56h4.03v4.03h-4.03z" style="animation:PlfXLEcr_draw 5333ms ease 1777ms forwards" stroke-dasharray="17 19" stroke-dashoffset="18"/><path class="cls-1" d="M391.84 68.32H108.16a6.59 6.59 0 00-6.59 6.6v326.66a6.59 6.59 0 006.59 6.59h283.68a6.59 6.59 0 006.59-6.59V74.92a6.59 6.59 0 00-6.59-6.6zm-16.24 219H128.43V95.19H375.6z" transform="translate(-89.65 -28.2)" style="animation:PlfXLEcr_draw 5333ms ease 2074ms forwards" stroke-dasharray="2141 2143" stroke-dashoffset="2142"/><path class="cls-1" d="M375.74 87.13H127a5.23 5.23 0 00-5.23 5.23v195.06a5.23 5.23 0 005.23 5.23h248.74a5.23 5.23 0 005.23-5.23V92.36a5.23 5.23 0 00-5.23-5.23zm-23.32 197.46H150.27a21.84 21.84 0 01-21.84-21.83v-144.4a21.84 21.84 0 0121.84-21.83h202.15a21.83 21.83 0 0121.83 21.83v144.4a21.83 21.83 0 01-21.83 21.83z" transform="translate(-89.65 -28.2)" style="animation:PlfXLEcr_draw 5333ms ease 2370ms forwards" stroke-dasharray="1751 1753" stroke-dashoffset="1752"/><path class="cls-1" d="M325.89 324.5v3.8h-83.95a1.84 1.84 0 00-2 1.62v11.94a1.85 1.85 0 002 1.63h83.95v6a1.85 1.85 0 002 1.62h49.72a1.85 1.85 0 002-1.62v-25a1.85 1.85 0 00-2-1.62h-49.7a1.85 1.85 0 00-2.02 1.63z" transform="translate(-89.65 -28.2)" style="animation:PlfXLEcr_draw 5333ms ease 2666ms forwards" stroke-dasharray="332 334" stroke-dashoffset="333"/><style data-made-with="vivus-instant">@keyframes PlfXLEcr_draw{to{stroke-dashoffset:0}}@keyframes PlfXLEcr_fade{0%,96.72131147540983%{stroke-opacity:1}to{stroke-opacity:0}}</style></svg>
+
+        </div>
+
+        <script src="./public/js/index.js" defer></script>
+    </body>
+</html>
---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var flipped = false;
+
+function flipSwitch(){
+    if (flipped) {
+        document.body.style.backgroundColor = 'white';
+        element.style.stroke = 'black';
+
+        flipped = false;
+    } else {
+        document.body.style.backgroundColor = 'black';
+        element.style.stroke = 'white';
+
+        flipped = true;
+    }
+}
+
+var element = document.getElementById('macintosh');
+element.addEventListener('click', flipSwitch);
